6869ec9d64bf8d34738b34a2fde1d95af7d84424
sonwezali <al1.sonmez.mi@gmail.com>
2023-10-02 00:52:53 +0300

    updated main.c
    created the first train set and random float generator

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-    printf("Hello GitHub!\n");
+float train[][2] = {
+    {0, 0},
+    {1, 2},
+    {2, 4},
+    {3, 6},
+    {4, 8},
+};
+
+float rand_float()
+{
+    return (float) rand() / (float) RAND_MAX;
+}
+
+int main() 
+{
+    // srand(time(NULL));
+    srand(3); // kept as 3 for testing purposes
+    // y = x*w;
+    float w = rand_float() * 10;
+
+    printf("%f\n", w);
     return 0;
 }
 
