cffdefb069edb547be8f780ae69bf896ccdeccfa
sonwezali <al1.sonmez.mi@gmail.com>
2023-10-03 23:10:23 +0300

    updated main.c

--- main.c
+++ main.c
@@ -17,12 +17,12 @@ float rand_float()
     return (float) rand() / (float) RAND_MAX;
 }
 
-float costFunc(float w)
+float costFunc(float w, float b)
 {
     float result = 0.0f;
     for (size_t i = 0; i < train_size; i++) {
         float x = train[i][0];
-        float y = x*w;
+        float y = x*w + b;
         float d = y - train[i][1];
         result += d*d;
     } 
@@ -32,19 +32,19 @@ float costFunc(float w)
 
 int main() 
 {
-    //srand(time(0));
-    srand(3); // kept as 3 for testing purposes
+    srand(time(0));
     // y = x*w;
-    float w = rand_float()*10;
+    float w = rand_float()*10.0f;
+    float b = rand_float()*5.0f;
 
     float eps = 1e-3;
     float rate = 1e-3;
 
-    printf("%f\n", costFunc(w));
+    printf("%f\n", costFunc(w, 0));
     for (size_t i = 0; i < 500; i++) {
-        float dCost = (costFunc(w + eps) - costFunc(w))/eps;
+        float dCost = (costFunc(w + eps, 0) - costFunc(w, 0))/eps;
         w -= rate*dCost; // Finite difference
-        printf("%f\n", costFunc(w));
+        printf("cost = %f, w = %f\n", costFunc(w, 0), w);
     }
     printf("---------------------------\n");
     printf("w: %f\n", w);
